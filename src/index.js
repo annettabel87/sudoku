1ae556c2fc8487d0de93902eaaccbcd4e4658b1b
annettabel87 <annettabel@rambler.ru>
2021-09-02 22:44:57 +0500

    feat: add find Empty Cell

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
+  const sizeMatrix = 9;
+  const cell = 3;
+
+function getEmptyCells(matrix) {
+  for (let i = 0; i < sizeMatrix; i++) {
+    for (let j = 0; j < sizeMatrix; j++) {
+      if (matrix[i][j] === 0) {
+      return ([i, j])
+      }
+    }
+  }
+}
+  
+  for (let i =0; i < 10; i++) {
+
+  }
 }
