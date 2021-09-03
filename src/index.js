bc0755dfa1d2d51b69e0a198b670d22711fc22dc
annettabel87 <annettabel@rambler.ru>
2021-09-03 15:23:37 +0500

    feat:add isValid

--- src/index.js
+++ src/index.js
@@ -1,19 +1,59 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
   const sizeMatrix = 9;
-  const cell = 3;
+  const area = 3;
 
-function getEmptyCells(matrix) {
-  for (let i = 0; i < sizeMatrix; i++) {
-    for (let j = 0; j < sizeMatrix; j++) {
-      if (matrix[i][j] === 0) {
-      return ([i, j])
+const getEmptyCells = (matrix) => {
+  for (let r = 0; r < sizeMatrix; r++) {
+    for (let c = 0; c < sizeMatrix; c++) {
+      if (matrix[r][c] === 0) {
+      return [r, c];
       }
     }
   }
+  return null;
 }
   
-  for (let i =0; i < 10; i++) {
+  const isValid = (number, position, matrix) => {
+    const [r, c] = position;
 
+    for (let i = 0; i < sizeMatrix; i++) {
+      if(matrix[i][c] === number && i != r) {
+        return false;
+      }      
+    }
+
+    for (let j = 0; j < sizeMatrix; j++) {
+      if(matrix[r][j] === number && j != c) {
+        return false;
+      }      
+    }
+
+    const startAreaRow = Math.floor(r/area) * area;
+    const startAreaColumn = Math.floor(c/area) * area;
+    for (let i = startAreaRow; i < startAreaRow + area; i++) {
+      for (let j = startAreaColumn; j < startAreaColumn + area; j++) {
+      if (matrix[i][j] === number && i != r && j != c) {
+        return false;
+      }
+    }
   }
+  return true;
+  }
+  const solve = () => {
+    const currentPosition = getEmptyCells(matrix);
+    if (currentPosition = null) {
+      return true;
+    }
+
+    for (let i = 1; i <= sizeMatrix; i++) {
+      let currentNumber = i;
+      let valid = isValid(currentNumber, currentPosition, matrix);
+    }
+    return false;
+  }
+
+  solve();
+
+  return matrix;
 }
